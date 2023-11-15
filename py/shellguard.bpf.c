72b5dd3982bad736c2e6da850588e0b8e7df176e
Michael Taggart <mtaggart@taggart-tech.com>
2023-11-15 13:23:11 -0800

    Working for ONE

--- py/shellguard.bpf.c
+++ py/shellguard.bpf.c
@@ -2,26 +2,29 @@
 #include <linux/sched.h>
 #include <linux/string.h>
 #include <uapi/linux/ptrace.h>
-#define MAXARG 1
+#define MAXARG 3
 #define ARGSIZE 128
 
 BPF_RINGBUF_OUTPUT(events, 16);
 
-// field:unsigned short common_type;	offset:0;	size:2;	signed:0;
-// field:unsigned char common_flags;	offset:2;	size:1;	signed:0;
-// field:unsigned char common_preempt_count;	offset:3;	size:1;
-// signed:0; field:int common_pid;	offset:4;	size:4;	signed:1;
+/*
+ ========EXECVE ARGS=========
+ field:unsigned short common_type;	offset:0;	size:2;	signed:0;
+ field:unsigned char common_flags;	offset:2;	size:1;	signed:0;
+ field:unsigned char common_preempt_count;	offset:3;	size:1;
+ signed:0; field:int common_pid;	offset:4;	size:4;	signed:1;
 
-// field:int __syscall_nr;	offset:8;	size:4;	signed:1;
-// field:const char * filename;	offset:16;	size:8;	signed:0;
-// field:const char *const * argv;	offset:24;	size:8;	signed:0;
-// field:const char *const * envp;	offset:32;	size:8;	signed:0
+ field:int __syscall_nr;	offset:8;	size:4;	signed:1;
+ field:const char * filename;	offset:16;	size:8;	signed:0;
+ field:const char *const * argv;	offset:24;	size:8;	signed:0;
+ field:const char *const * envp;	offset:32;	size:8;	signed:0
+*/
 
 struct event_t {
   int pid;
   int uid;
   char comm[TASK_COMM_LEN];
-  char argv[MAXARG][ARGSIZE];
+  char arg[ARGSIZE];
 };
 
 TRACEPOINT_PROBE(syscalls, sys_enter_execve) {
@@ -33,23 +36,19 @@ TRACEPOINT_PROBE(syscalls, sys_enter_execve) {
   event.pid = bpf_get_current_pid_tgid() >> 32;
   event.uid = bpf_get_current_uid_gid();
 
-  const char *arg = NULL;
-  bpf_probe_read(&arg, sizeof(arg), &args->argv[1]);
-  bpf_probe_read_user_str(event.argv, sizeof(event.argv), arg);
-
-  // bpf_probe_read((void *)&event->argv, sizeof(event->argv), &arg[1]);
-
   bpf_trace_printk("%s", event.comm);
   bpf_trace_printk("%d", event.pid);
-  bpf_trace_printk("%s", event.argv);
-  int i = 0;
-  // while (i < MAXARG) {
-  //   if (bpf_probe_read_user_str((void *)event->argv, ctxIZE, ctx->argv[i])
-  //   <
-  //       0) {
-  //     break;
-  //   }
-  // }
+  // bpf_trace_printk("%s", event.argv[1]);
+  int i = 1;
+  while (i < 2) {
+    const char *arg = NULL;
+    bpf_probe_read(&arg, sizeof(arg), &args->argv[i]);
+    int res = bpf_probe_read_user_str(event.arg, sizeof(event.arg), arg);
+    if (res < 0) {
+      break;
+    }
+    i++;
+  }
   events.ringbuf_output(&event, sizeof(event), 0);
   return 0;
 }
--- py/shellguard.py
+++ py/shellguard.py
@@ -1,8 +1,31 @@
 from bcc import BPF
 import sys
 import time
+import ctypes as ct
+
+ARGSIZE = 128
+MAXARG = 3
+TASK_COMM_LEN = 16
+
+
+class EventArg(ct.Structure):
+    _fields_ = [
+        ("pid", ct.c_uint),
+        ("uid", ct.c_uint),
+        ("comm", ct.c_char * TASK_COMM_LEN),
+        ("argv", ct.c_char * ARGSIZE),
+    ]
+
+class EventType:
+    EVENT_ARG = 0
+    EVENT_RET = 1
+
 
 shells = ["bash", "fish", "zsh", "sh", "dash"]
+shells = ["bash", "fish", "zsh", "sh", "dash"]
+shells = ["bash", "fish", "zsh", "sh", "dash"]
+shells = ["bash", "fish", "zsh", "sh", "dash"]
+
 with open("./shellguard.bpf.c") as f:
     prog_text = f.read()
 
@@ -13,9 +36,9 @@ b = BPF(text=prog_text)
 def callback(ctx, data, size):
     event = b["events"].event(data)
     comm = event.comm.decode()
-    argv = event.argv.decode()
+    arg = event.arg.decode()
     if comm.split("/")[-1] in shells:
-        print(f"New Shell Exec: {event.pid} [{event.uid}] {comm} {argv}")
+        print(f"New Shell Exec: {event.pid} [{event.uid}] {comm} {arg}")
 
 
 b["events"].open_ring_buffer(callback)
@@ -27,6 +50,6 @@ try:
     while 1:
         b.ring_buffer_consume()
         time.sleep(0.5)
-        b.trace_print()
+        # b.trace_print()
 except KeyboardInterrupt:
     sys.exit()
