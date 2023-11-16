c9e22c00e892e042eaf1923fb78651a7481c3bd6
Michael Taggart <mtaggart@taggart-tech.com>
2023-11-15 19:42:37 -0800

    Working Python Shellguardian!

--- py/shellguard.bpf.c
+++ py/shellguard.bpf.c
@@ -2,7 +2,7 @@
 #include <linux/sched.h>
 #include <linux/string.h>
 #include <uapi/linux/ptrace.h>
-#define MAXARG 3
+#define MAXARG 20
 #define ARGSIZE 128
 
 BPF_RINGBUF_OUTPUT(events, 16);
@@ -20,35 +20,68 @@ BPF_RINGBUF_OUTPUT(events, 16);
  field:const char *const * envp;	offset:32;	size:8;	signed:0
 */
 
+enum event_type { EVENT_ARG, EVENT_RET };
+
 struct event_t {
   int pid;
   int uid;
+  enum event_type type;
   char comm[TASK_COMM_LEN];
   char arg[ARGSIZE];
 };
 
 TRACEPOINT_PROBE(syscalls, sys_enter_execve) {
 
+  // Create our event struct to hold exec info
   struct event_t event = {};
 
+  // Set event type
+  // We will fire one of these off for each arg
+  // Why? Because the args are long and the eBPF stack is only 512b
+  event.type = EVENT_ARG;
+
+  // Read the command fileneme into our struct
   bpf_probe_read_user_str(event.comm, sizeof(event.comm), args->filename);
 
+  // Read the PID/UID in
   event.pid = bpf_get_current_pid_tgid() >> 32;
   event.uid = bpf_get_current_uid_gid();
 
+  // Add trace printing for debug purposes
   bpf_trace_printk("%s", event.comm);
   bpf_trace_printk("%d", event.pid);
-  // bpf_trace_printk("%s", event.argv[1]);
+  bpf_trace_printk("%s", event.arg);
+
+  // Now for the arg loop
+  // eBPF doesn't do for loops, so this has to be a while
+  // We start at 1 because we already have the filename
   int i = 1;
-  while (i < 2) {
+  while (i < MAXARG) {
+    // Initialize the safe pointer for our string
     const char *arg = NULL;
+
+    // Read from the context argv pointer
     bpf_probe_read(&arg, sizeof(arg), &args->argv[i]);
+    // Attempt to writte to our event struct from the save pointer.
+    // If it fails, we're done with the loop.
     int res = bpf_probe_read_user_str(event.arg, sizeof(event.arg), arg);
     if (res < 0) {
       break;
     }
+    // Submit to the Ring Buffer
+    events.ringbuf_output(&event, sizeof(event), 0);
     i++;
   }
+  return 0;
+}
+
+int kretprobe__sys_execve(struct pt_regs *ctx) {
+  struct event_t event = {};
+  event.pid = bpf_get_current_pid_tgid() >> 32;
+  event.uid = bpf_get_current_uid_gid();
+  bpf_get_current_comm(&event.comm, sizeof(event.comm));
+  event.type = EVENT_RET;
   events.ringbuf_output(&event, sizeof(event), 0);
+
   return 0;
 }
--- py/shellguard.py
+++ py/shellguard.py
@@ -2,28 +2,29 @@ from bcc import BPF
 import sys
 import time
 import ctypes as ct
+from collections import defaultdict
+from psutil import Process
 
 ARGSIZE = 128
 MAXARG = 3
 TASK_COMM_LEN = 16
 
 
-class EventArg(ct.Structure):
+class Event(ct.Structure):
     _fields_ = [
         ("pid", ct.c_uint),
         ("uid", ct.c_uint),
+        ("type", ct.c_int),
         ("comm", ct.c_char * TASK_COMM_LEN),
-        ("argv", ct.c_char * ARGSIZE),
+        ("arg", ct.c_char * ARGSIZE),
     ]
 
 class EventType:
     EVENT_ARG = 0
     EVENT_RET = 1
 
+execs = defaultdict(list)
 
-shells = ["bash", "fish", "zsh", "sh", "dash"]
-shells = ["bash", "fish", "zsh", "sh", "dash"]
-shells = ["bash", "fish", "zsh", "sh", "dash"]
 shells = ["bash", "fish", "zsh", "sh", "dash"]
 
 with open("./shellguard.bpf.c") as f:
@@ -34,12 +35,27 @@ b = BPF(text=prog_text)
 
 
 def callback(ctx, data, size):
-    event = b["events"].event(data)
-    comm = event.comm.decode()
-    arg = event.arg.decode()
-    if comm.split("/")[-1] in shells:
-        print(f"New Shell Exec: {event.pid} [{event.uid}] {comm} {arg}")
 
+    event = ct.cast(data, ct.POINTER(Event)).contents
+    comm = event.comm.decode()
+    pid = event.pid
+    uid = event.uid
+    if event.type == EventType.EVENT_ARG:
+        execs[event.pid].append(event.arg.decode())
+    elif event.type == EventType.EVENT_RET:
+        argv = " ".join(execs[pid])
+        if comm.split("/")[-1] in shells:
+            if uid > 0 and uid < 1000:
+                msg = "UNAUTHORIZED SHELL DETECTED: "
+                if "-k" in sys.argv:
+                    proc = Process(pid)
+                    print(f"KILL MODE ACTIVATED ON {pid}")
+                    proc.kill()
+            else:
+                msg = "New Shell Executed: "
+            print(f"{msg} {pid} [{uid}] {comm} {argv}")
+            
+            del execs[pid]
 
 b["events"].open_ring_buffer(callback)
 
@@ -48,8 +64,9 @@ print("Printing execs() calls, ctrl-c to exit.")
 
 try:
     while 1:
-        b.ring_buffer_consume()
-        time.sleep(0.5)
+        # b.kprobe_poll()
+        b.ring_buffer_poll()
         # b.trace_print()
+        time.sleep(0.5)
 except KeyboardInterrupt:
     sys.exit()
